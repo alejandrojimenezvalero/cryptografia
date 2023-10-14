f2be41956317bd0353bb6b89ff3a725e2eae7593
edu454471 <100454471@alumnos.uc3m.es>
2023-10-14 16:31:08 +0200

    wip

--- cipher.py
+++ cipher.py
@@ -1,10 +1,12 @@
-
 from cryptography.fernet import Fernet
+
 
 def initialize():
     key = b'GX0wOFAFYIohW8EIKfIyc0c4w-NMQg5nT2cDqr7zeHk='
     cipher_suite = Fernet(key)
     return cipher_suite
+
+
 def password_encryption(password):
     # we generate the key
     # we create out a Fernet object
@@ -13,14 +15,8 @@ def password_encryption(password):
     encrypted_pass = cipher_suite.encrypt(password.encode()).decode()
     return encrypted_pass
 
+
 def password_decryption(encrypted_value):
     cipher_suite = initialize()
     decrypted_value = cipher_suite.decrypt(encrypted_value).decode()
     return decrypted_value
-
-pw = input("Introduce tu contraseña: ")
-
-hashpw = password_encryption(pw)
-print("Tu contraseña encriptada es la siguiente: ", hashpw)
-print("Tu contraseña es: ",password_decryption(hashpw))
-
